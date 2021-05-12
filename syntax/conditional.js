3d50cd13e907cca42c0d53b3f6f3a9ea9a9036ff
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2021-05-12 22:18:35 +0900

    JavaScript_Syntax

---
+++ syntax/conditional.js
@@ -0,0 +1,11 @@
+// 조건문
+
+console.log("A"); // A
+console.log("B"); // B
+if (false) {
+  // 조건에 따라 C1, C2 둘 중에 하나 출력
+  console.log("C1");
+} else {
+  console.log("C2");
+}
+console.log("D"); // D
