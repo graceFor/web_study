d610742c743158be75e234d566a3a673c839c16d
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2021-05-12 22:34:11 +0900

    input the console

--- syntax/conditional.js
+++ syntax/conditional.js
@@ -1,11 +1,13 @@
 // 조건문
 
-console.log("A"); // A
-console.log("B"); // B
-if (false) {
-  // 조건에 따라 C1, C2 둘 중에 하나 출력
+var args = process.argv;
+console.log(args[2]); // 콘솔에서 입력 값은 args[2]부터!
+console.log("A");
+console.log("B");
+if (args[2] === "1") {
+  // 콘솔에서의 입력값이 1이면 C1 출력
   console.log("C1");
 } else {
   console.log("C2");
 }
-console.log("D"); // D
+console.log("D");
