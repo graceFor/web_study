bd83880941e7e326aefc047fa3a7d724e3a8bff3
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2021-05-16 16:59:47 +0900

    App_Create UI

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ function templateHTML(title, list, body) {
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
+    <a href="/create">create</a>
     ${body}
   </body>
   </html>
@@ -34,6 +35,7 @@ var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
+  console.log(pathname);
   if (pathname === "/") {
     if (queryData.id === undefined) {
       // 메인 Home 즉, query string 값 존재하지 않을 때
@@ -57,6 +59,28 @@ var app = http.createServer(function (request, response) {
         });
       });
     }
+  } else if (pathname === "/create") {
+    fs.readdir("./data", function (error, filelist) {
+      var title = "WEB - create";
+      var list = templateList(filelist);
+      var template = templateHTML(
+        title,
+        list,
+        `
+        <form action="http://localhost:3000/process_create" method="post">
+          <p><input type="text" name="title" placeholder="title"></p>
+          <p>
+            <textarea name="description" placeholder="description"></textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+      `
+      );
+      response.writeHead(200);
+      response.end(template);
+    });
   } else {
     response.writeHead(404);
     response.end("Not found");
