50057002ff770d152523509a23e5d2bcc44ac02a
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2021-05-17 15:37:23 +0900

    App_Receiving data transmitted through POST method

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var http = require("http"); //require => 요구하다.
 var fs = require("fs");
+var qs = require("querystring");
 var url = require("url"); // url이라는 모듈을 사용할 것이라고 node.js에게 알려줌
 // url => 모듈 url을 말함
 
@@ -68,7 +69,7 @@ var app = http.createServer(function (request, response) {
         title,
         list,
         `
-        <form action="http://localhost:3000/process_create" method="post">
+        <form action="http://localhost:3000/create_process" method="post">
           <p><input type="text" name="title" placeholder="title"></p>
           <p>
             <textarea name="description" placeholder="description"></textarea>
@@ -82,6 +83,26 @@ var app = http.createServer(function (request, response) {
       response.writeHead(200);
       response.end(template);
     });
+  } else if (pathname === "/create_process") {
+    var body = "";
+    // 사용자가 요청한 정보 안에 POST 정보가 있음
+    request.on("data", function (data) {
+      // 웹브라우저가 POST 방식으로 데이터를 전송할 때, 데이터가 많으면 그 데이터를 한번에 처리할 수 없어서 node.js에서는 POST 방식으로 전송되는 데이터가 많을 경우를 대비한 방법
+      // 정보가 조각조각 들어옴
+      body = body + data;
+      // body 데이터에 callback이 살행될 때마다 data를 추가함
+    });
+    request.on("end", function (data) {
+      // 더이상 들어올 정보가 없으면 end 다음에 들어올 콜백함수 호출
+      // 즉, end에 해당되는 callback이 실행됐을 때, 정보수신 끝났다고 생각하면 됨
+      var post = qs.parse(body);
+      console.log(post);
+      var title = post.title;
+      var description = post.description;
+    });
+
+    response.writeHead(200);
+    response.end("success");
   } else {
     response.writeHead(404);
     response.end("Not found");
