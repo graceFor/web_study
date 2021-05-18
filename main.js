fb047b0c84d461efcb7cb6ab97f61fb3f34846a0
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2021-05-18 11:08:44 +0900

    App_Update

--- main.js
+++ main.js
@@ -116,7 +116,6 @@ var app = http.createServer(function (request, response) {
       });
     });
   } else if (pathname === "/update") {
-    // query string 값 존재 => id 값 있는 경우
     fs.readdir("./data", function (error, filelist) {
       fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
         var title = queryData.id;
@@ -126,16 +125,16 @@ var app = http.createServer(function (request, response) {
           list,
           `
         <form action="/update_process" method="post">
-        <input type="hidden" name="id" value =${title}>
-          <p><input type="text" name="title" value =${title}></p>
+          <input type="hidden" name="id" value="${title}">
+          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
           <p>
-            <textarea name="description">${description}</textarea>
+            <textarea name="description" placeholder="description">${description}</textarea>
           </p>
           <p>
             <input type="submit">
           </p>
         </form>
-      `,
+        `,
           `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
         );
         response.writeHead(200);
@@ -144,25 +143,23 @@ var app = http.createServer(function (request, response) {
     });
   } else if (pathname === "/update_process") {
     var body = "";
-    // 사용자가 요청한 정보 안에 POST 정보가 있음
     request.on("data", function (data) {
-      // 웹브라우저가 POST 방식으로 데이터를 전송할 때, 데이터가 많으면 그 데이터를 한번에 처리할 수 없어서 node.js에서는 POST 방식으로 전송되는 데이터가 많을 경우를 대비한 방법
-      // 정보가 조각조각 들어옴
       body = body + data;
-      // body 데이터에 callback이 살행될 때마다 data를 추가함
     });
-    request.on("end", function (data) {
-      // 더이상 들어올 정보가 없으면 end 다음에 들어올 콜백함수 호출
-      // 즉, end에 해당되는 callback이 실행됐을 때, 정보수신 끝났다고 생각하면 됨
+    request.on("end", function () {
       var post = qs.parse(body);
-      console.log(post);
+      // id = 기존의 파일 이름
       var id = post.id;
+      // title = 바뀐 파일 이름
       var title = post.title;
+      // description = 바뀐 파일 내용
       var description = post.description;
-      //   fs.writeFile(`data/${title}`, description, "utf8", function (err) {
-      //     response.writeHead(302, { Location: `/?id=${title}` });
-      //     response.end();
-      //   });
+      fs.rename(`data/${id}`, `data/${title}`, function (error) {
+        fs.writeFile(`data/${title}`, description, "utf8", function (err) {
+          response.writeHead(302, { Location: `/?id=${title}` });
+          response.end();
+        });
+      });
     });
   } else {
     response.writeHead(404);
