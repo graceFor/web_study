8db4f9b1ccb295c89e7a788c94466ab5868f536c
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2021-05-19 16:43:55 +0900

    APP_Secure

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var url = require("url");
 var qs = require("querystring");
 var template = require("./lib/template.js");
 var path = require("path");
+var sanitizeHtml = require("sanitize-html");
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
@@ -32,14 +33,16 @@ var app = http.createServer(function (request, response) {
         var filteredId = path.parse(queryData.id).base;
         fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
           var title = queryData.id;
+          var sanitizedTitle = sanitizeHtml(title);
+          var sanitizedDescription = sanitizeHtml(description);
           var list = template.list(filelist);
           var template = template.html(
             title,
             list,
-            `<h2>${title}</h2>${description}`,
+            `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
             `<a href="/create">create</a> 
-            <a href="/update?id=${title}">update</a><form action="delete_process" method="post">
-            <input type="hidden" name="id" value="${title}">
+            <a href="/update?id=${sanitizedTitle}">update</a><form action="delete_process" method="post">
+            <input type="hidden" name="id" value="${sanitizedTitle}">
             <input type="submit" value="delete">
           </form>`
           );
