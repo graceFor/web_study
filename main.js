4129641a57cefe397dd6b57a2bac7fd2cbd2683a
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2021-05-19 15:08:44 +0900

    Node.js_Module

--- main.js
+++ main.js
@@ -4,35 +4,7 @@ var qs = require("querystring");
 var url = require("url"); // url이라는 모듈을 사용할 것이라고 node.js에게 알려줌
 // url => 모듈 url을 말함
 
-var template = {
-  html: function (title, list, body, control) {
-    return `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      ${list}
-      ${control}
-      ${body}
-    </body>
-    </html>
-    `;
-  },
-  list: function (filelist) {
-    var list = "<ul>";
-    var i = 0;
-    while (i < filelist.length) {
-      list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-      i = i + 1;
-    }
-    list = list + "</ul>";
-    return list;
-  },
-};
+var template = require("./lib/template");
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
