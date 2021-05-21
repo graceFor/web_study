69fed372d7a083a0f6007c221a3eb81687fce1dc
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2021-05-21 11:41:08 +0900

    Node.js_Mysql module

--- main.js
+++ main.js
@@ -5,6 +5,20 @@ var qs = require("querystring");
 var template = require("./lib/template.js");
 var path = require("path");
 var sanitizeHtml = require("sanitize-html");
+var mysql = requier("mysql2");
+
+// mysql 서버에 접속하기 위한 setting
+var db = mysql.createConnection({
+  // host = 데이터베이스 서버가 어떤 컴퓨터에 있는지
+  // localhost = node.js와 mysql 서버가 같은 컴퓨터에 있음
+  host: "localhost",
+  user: "root",
+  password: "gustns50",
+  database: "opentutorials",
+});
+
+//실제 접속
+db.connect();
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
