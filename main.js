9251806883fcb03d3e9fe6bc8b63e7a1ba60ad67
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2021-05-24 20:53:12 +0900

    Node.js_author create, update

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ var template = require("./lib/template.js");
 var path = require("path");
 var db = require("./lib/db.js");
 var topic = require("./lib/topic.js");
+var author = require("./lib/author.js");
+const { authorSelect } = require("./lib/template.js");
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
@@ -28,6 +30,14 @@ var app = http.createServer(function (request, response) {
     topic.update_process(request, response);
   } else if (pathname === "/delete_process") {
     topic.delete_process(request, response);
+  } else if (pathname === "/author") {
+    author.home(request, response);
+  } else if (pathname === "/author/create_process") {
+    author.create_process(request, response);
+  } else if (pathname === "/author/update") {
+    author.update(request, response);
+  } else if (pathname === "/author/update_process") {
+    author.update_process(request, response);
   } else {
     response.writeHead(404);
     response.end("Not found");
