385fb2fd7786636ca06d2cde6985f5b20bf8a385
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2021-06-23 19:25:05 +0900

    useState

--- src/Counter.js
+++ src/Counter.js
@@ -1,14 +1,10 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    //state의 초것값 설정하기
-    this.state = {
-      number: 0,
-      fixedNumber: 0,
-    };
-  }
+  state = {
+    number: 0,
+    fixedNumber: 0,
+  };
   render() {
     const { number, fixedNumber } = this.state; // state롤 조화할 때는 this.state로 조화합니다.
     return (
@@ -18,8 +14,10 @@ class Counter extends Component {
         <button
           //onClick을 통해 버론이 클릭되었울 때 호출할 함수룰 지정합니다.
           onClick={() => {
-            // this.setState틀 사용하여 state에 새로운 값울 넣울 수 있습니다.
-            this.setState({ number: number + 1 });
+            this.setState({ number: number + 1 }, () => {
+              console.log("방금 setState가 호출됨");
+              console.log(this.state);
+            });
           }}
         >
           +1
