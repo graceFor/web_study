4724567388ae5fab561b053c024327d921dcf4e3
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2021-06-23 16:06:50 +0900

    ClassComponent

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  const name = "리액트";
-  return (
-    <>
-      {/* 다은 이렇게 작성합니다. */}
-      <div
-        className="react" // 사작 태그를 여러 쿨로 작성하게 된다면 여기에 주석을 작성할 수 었습니다.
-      >
-        {name}
-      </div>
-
-      <input />
-    </>
-  );
+class App extends Component {
+  render() {
+    const name = "react";
+    return <div className="react">{name}</div>;
+  }
 }
 
 export default App;
