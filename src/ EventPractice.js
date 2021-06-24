389d916590bf3fbad20c1a25abcaa51f854ac293
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2021-06-25 01:07:03 +0900

    Event Handeling

--- src/ EventPractice.js
+++ src/ EventPractice.js
@@ -2,35 +2,47 @@ import React, { Component } from "react";
 
 class EventPractice extends Component {
   state = {
+    username: "",
     message: "",
   };
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
 
-  handleChange(e) {
+  handleChange = (e) => {
     this.setState({
-      message: e.target.value,
+      [e.target.name]: e.target.value,
+      // username: e.target.value,
+      // message : e.target.value
     });
-  }
-  handleClick(e) {
-    alert(this.state.message);
+  };
+  handleClick = () => {
+    alert(this.state.username + " : " + this.state.message);
     this.setState({
+      username: "",
       message: " ",
     });
-  }
+  };
+  handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      this.handleClick();
+    }
+  };
   render() {
     return (
       <div>
         <h1>이벤트연습</h1>
         <input
           tpye="text"
+          name="username"
+          placeholder="사용자명"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <input
+          tpye="text"
           name="message"
           placeholder="아무거나 입력해보세요"
           value={this.state.message}
           onChange={this.handleChange}
+          onKeyPress={this.handleKeyPress}
         />
         <button onClick={this.handleClick}>확인</button>
       </div>
