bb1da6e9f49aeebcce16f4abd5633b9cbc4d992e
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2021-06-25 21:12:28 +0900

    ref

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 
-import EventPractice from "./ EventPractice";
+import ScrollBox from "./ScrollBox";
 
-const App = () => {
-  return <EventPractice />;
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
