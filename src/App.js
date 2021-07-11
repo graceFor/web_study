3f54c305b005869cd44365195376cf35c7944709
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2021-07-11 21:23:12 +0900

    API

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=1e5c4a1c1f9a480db12b12144c75134f',
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
-  );
+  return <Route path="/:category?" component={NewsPage} />;
 };
 
 export default App;
