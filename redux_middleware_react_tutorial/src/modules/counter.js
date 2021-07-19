5b907b091d5c870019af6f34f9fe5984a3c2121e
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2021-07-19 23:33:07 +0900

    make Middleware

--- redux_middleware_react_tutorial/src/modules/counter.js
+++ redux_middleware_react_tutorial/src/modules/counter.js
@@ -1,13 +1,17 @@
 import { createAction, handleActions } from "redux-actions";
 
+// 액션 타입 정의하기
 const INCREASE = "counter/INCREASE";
 const DECREASE = "counter/DECREASE";
 
+// 액션 생성 함수 만들기
 export const increase = createAction(INCREASE);
 export const decrease = createAction(DECREASE);
 
+// 초기상태
 const initialState = 0; // 상태는 꼭 객체일 필요 없음
 
+// 리듀서 함수 만들기
 const counter = handleActions(
   {
     [INCREASE]: (state) => state + 1,
