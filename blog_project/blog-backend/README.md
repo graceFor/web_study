84cf15f520f5c83cdacc4b7ee222ec52419417a7
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2021-07-29 13:57:04 +0900

    Mongoose

--- blog_project/blog-backend/README.md
+++ blog_project/blog-backend/README.md
@@ -1,3 +1,20 @@
 # blog_project_backend
 
 - Node.js의 프레임워크인 Koa.js를 사용
+- MongoDB 기반 ODB 라이브러리인 mongoose를 이용하여 서버에서 직접 데이터를 추가 • 조회 • 삭제 • 수정
+
+### .env
+
+- PORT (optional): port number of local server
+- MongoDB URI(required): MongoDB connection URI used to connect to a MongoDB deployment
+
+#### Example .env file
+
+```text
+PORT=4000
+MongoDB URI="{ mongodb://localhost:27017/[dbname] }"
+```
+
+## User stories
+
+- Normal users
--- blog_project/blog-backend/src/index.js
+++ blog_project/blog-backend/src/index.js
@@ -1,8 +1,26 @@
+require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
-
+const mongoose = require('mongoose');
 const api = require('./api');
+
+// 비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
+// eslint-disable-next-line no-undef
+const { PORT, MONGO_URI } = process.env;
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 const app = new Koa();
 const router = new Router();
@@ -16,6 +34,9 @@ app.use(bodyParser());
 //app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log('Listenting to port 4000');
+// PORT가 지정되어 있지 않다면 4000을 사용
+const port = PORT || 4000;
+
+app.listen(port, () => {
+  console.log('Listenting to port %d', port);
 });
