3944a0f1a9fb676f69dc5710e9238a4522c09e42
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2021-07-29 12:05:57 +0900

    RESTAPI

--- blog_project/blog-backend/src/index.js
+++ blog_project/blog-backend/src/index.js
@@ -1,5 +1,7 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+
 const api = require('./api');
 
 const app = new Koa();
@@ -7,6 +9,9 @@ const router = new Router();
 
 // 라우터 설정
 router.use('/api', api.routes()); //api
+
+// 라우터 적용하기 전에 bodyParser 적용
+app.use(bodyParser());
 
 //app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
